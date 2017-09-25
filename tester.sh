6c273710f8d2fe77a407f14c9ac77c25faacdf0f
Josh Cox <josh@webhosting.coop>
2017-09-25 10:56:47 -0500

    exports

--- tester.sh
+++ tester.sh
@@ -10,6 +10,10 @@ chmod +x $TMP/tester.sh
 #for i in $(grep -v '^#' $INVENTORY);do echo $i|cut --output-delimiter=' ' -f1,2,3,4 -d','|awk '{print "ssh -p " $3 " " $1 "@" $2 " \"uname -a\"" }'>>$TMP/tester.sh ;done
 while IFS="," read REMOTE_USER REMOTE_HOST REMOTE_PORT REMOTE_PATH
 do
+    export REMOTE_USER=$REMOTE_USER
+    export REMOTE_HOST=$REMOTE_HOST
+    export REMOTE_PORT=$REMOTE_PORT
+    export REMOTE_PATH=$REMOTE_PATH
     echo  "ssh $REMOTE_USER@$REMOTE_HOST -p $REMOTE_PORT \"uname -a\"">>$TMP/tester.sh
 done < $INVENTORY
 echo " "
